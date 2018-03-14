398a0c08069f3d7362bd9d761950bcb788dc713f
Jorge Toledo <jorge.toledo@gmail.com>
2018-03-13 23:24:53 -0300

    Up myframework

---
+++ app/database.php
@@ -0,0 +1,16 @@
+<?php
+return [
+  /* Options (mysql, sqlite) */
+'driver' => 'mysql',
+'sqlite' => [
+  'database' => 'database.db'
+],
+'mysql' => [
+  'host' => 'localhost',
+  'database' => '',
+  'user' => '',
+  'pass' => '',
+  'charset' => 'utf8',
+  'collation' => 'utf8_unicode_ci'
+  ]
+];
